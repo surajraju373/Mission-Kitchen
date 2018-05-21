e7ff6d513a8377fd7409100fb3d5f7c703202dab
surajraju373 <31279985+surajraju373@users.noreply.github.com>
2018-05-21 09:09:11 +0530

    Add files via upload
    Sensors used: 
       To find out the amount of groceries left: Ultrasonic sensor, Load cell
       To gas leakage and fire accidents if any: MQ-6
    Database used: Firebase
    Technologies: Internet of Things
    
    Working:  The real time readings of sensor(analog values) are calculated from arduino code and this data is continuously sent to a serial port of the system. That serial data is read by the python client and it(python application) takes care of pushing the data to cloud. This data from cloud can be retrieved into a mobile application.
    
    Applications: Day-to-day analysis of usage, alerts is there is shortage of sugar or something, generates a shopping list automatically... :)

---
+++ gas_leakage_avoidance.ino
@@ -0,0 +1,51 @@
+/*
+www.geekstips.com
+MQ4 Gas Sensor - Arduino tutorial
+*/
+#include<Servo.h>
+int servoPin= D0;
+Servo servo;
+int servoAngle=0;
+const int gasPin = A0; //GAS sensor output pin to Arduino analog A0 pin
+
+void setup(){
+  Serial.begin(9600); //Initialize serial port - 9600 bps
+  servo.attach(servoPin);
+  pinMode(D1,HIGH);
+}
+
+void loop(){
+float  reading = analogRead(gasPin);
+  
+  if(reading>170 && reading<250)
+  {
+   digitalWrite(D1,HIGH);
+   delay(500);
+   digitalWrite(D1,LOW);
+   delay(500);
+  }
+  
+  
+  else if(reading>250)
+ {
+    Serial.print(reading);
+    Serial.print("\t");
+    Serial.println("Gas leaking");
+    servo.write(90);
+    delay(1000);
+    for(servoAngle=0;servoAngle<180;servoAngle=servoAngle+10)
+    {
+      servo.write(servoAngle);
+      delay(50);
+    }
+    for(servoAngle=180;servoAngle>0;servoAngle=servoAngle-10)
+    {
+    servo.write(servoAngle);
+    delay(50);
+    }
+
+ }
+  
+  delay(1000); // Print value every 1 sec.
+  
+}
